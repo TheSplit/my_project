ba26ed9ad25f956699eae1dd069342b219563731
Szymon Jamróz <trolololox02@wp.pl>
2024-12-04 09:40:00 +0000

    commir

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,31 @@
+use std::cell::RefCell;    
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
+    let a = String::from("Siema");
+    let b = &a;
+    let c = a;
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) -> String {
+    MSG.with(|msg| {
+        *msg.borrow_mut() = new_msg
+    });
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|msg| msg.borrow().clone())
+}
+
+
+// ===================================
+// new_msg: String
+// msg.borrow_mut(): &mut String
+// *&mut String == String
+// String == String
